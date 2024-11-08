8e43363ff1f257ba682da9e3a04afaa077fa8f9d
David Cano <dcanoea@fpvirtualaragon.es>
2024-11-08 22:53:01 +0100

    apartado 2 - lectura con SAX fin

---
+++ AD_Tarea02/src/tarea02/SAXLectura.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tarea02;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import org.xml.sax.SAXException;
+
+/**
+ *
+ * @author David Cano Escario
+ */
+public class SAXLectura {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            SAXParserFactory factory = SAXParserFactory.newInstance(); 
+            SAXParser parser = factory.newSAXParser();
+            
+            SAXLibroHandler handler = new SAXLibroHandler();
+            parser.parse("src\\resources\\libros.xml", handler);
+            
+        } catch (ParserConfigurationException ex) {
+            Logger.getLogger(SAXLectura.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SAXException ex) {
+            Logger.getLogger(SAXLectura.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(SAXLectura.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+}
