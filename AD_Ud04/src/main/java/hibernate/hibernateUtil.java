6236b61369beeefbe0c45281470d765a98adf7bc
David Cano <dcanoea@fpvirtualaragon.es>
2025-01-26 21:21:56 +0100

    conexion con Hibernate

---
+++ AD_Ud04/src/main/java/hibernate/hibernateUtil.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package hibernate;
+
+/**
+ *
+ * @author David Cano Escario
+ */
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+public class hibernateUtil {
+    private static final SessionFactory sessionFactory;
+    static {
+        try {
+            sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+    public static void shutdown() {
+        getSessionFactory().close();
+    }
+}
